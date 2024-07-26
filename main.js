fbd9995b2d0ed15c8c3830b02ca26babec2228d4
nicolads <111922549+noelzada@users.noreply.github.com>
2024-07-26 11:08:46 -0300

    a

---
+++ main.js
@@ -0,0 +1,23 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "Você gosta de bolo?",
+        alternativas: [
+            {
+                texto: "Alternativa A"
+            },
+            {
+                texto: "Alternativa B"
+            }]
+    },
+    pergunta2{
+
+    }
+]
+
+ div class="caixa-principal"
+    <h1> Você Decide o Futuro da IA</h1>
+    <div class="caixa-perguntas"></div>
+    <div class="caixa-alternativas"></div>
+    <div class="caixa-resultado"></div>
+        <p class="texto-resultado"></p>
+        </div>
